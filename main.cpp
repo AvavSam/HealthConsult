ae956a3db429e676b16df06599f00c2955131540
Avav Sam <avav2608005@gmail.com>
2023-12-20 10:04:58 +0800

    Update to the C++ health consultation program.
    - Done adding all feature
    - Fixed error looping

--- main.cpp
+++ main.cpp
@@ -9,6 +9,18 @@ struct RekamMedis
     string penyakit;
     string tanggal;
 };
+struct Reservasi
+{
+    string dokter;
+    string waktukonsul;
+    string metodebayar;
+    string hargakonsul;
+};
+
+const int MAX_RESERVATIONS = 10;
+
+string daftardokter[] = {"dr. Anisa putri \t\t(Dokter Umum)", "dr. Novi dwiyanti \t\t(Dokter Umum)", "dr. Kelvin yuwanda \t\t(Dokter Umum)", "dr. Raisya, Sp.A \t\t(Dokter Anak)", "dr. Dean, Sp.KG \t\t(Dokter Konservasi Gigi)", "dr. Vector, Sp.PM \t\t(Dokter Penyakit Mulut)"};
+string daftarwaktukonsul[] = {"Senin \t09:00", "Senin \t17:25", "Selasa \t08:00", "Kamis \t09:00", "Kamis \t12:50", "Jumat \t15:00"};
 
 void garis();
 int utama();
@@ -39,7 +51,6 @@ int login() // Tampilan login, DONE
         cin >> uid;
         cout << "Masukkan Password  \t: ";
         cin >> pass;
-        garis();
         if ((strcmp(idlogin, uid) == 0 && strcmp(pwlogin, pass) == 0))
         {
             utama();
@@ -98,13 +109,13 @@ int utama() // Menu utama, DONE
         break;
     case '3':
         konsultasi();
-        break;
+        return 0;
     case '4':
         riwayat_periksa();
         break;
     case '5':
         logout();
-        break;
+        return 0;
     default:
         cout << "Maaf, Input tidak Valid!" << endl;
         garis();
@@ -141,7 +152,7 @@ int tentang_kami() // Menu tentang kami, tambah penjelasan fitur fitur
 int obat() // Menu pemilihan obat, kurang cara beli obat yang dipilih
 {
     char pilihan;
-    int harga1, harga2, harga3, harga4, harga5;
+    int harga1 = 0, harga2 = 0, harga3 = 0, harga4 = 0, harga5 = 0;
     cout << "Daftar Obat" << endl;
     garis();
     cout << "[1] Paracetamol \tRp20000" << endl;
@@ -165,66 +176,248 @@ int obat() // Menu pemilihan obat, kurang cara beli obat yang dipilih
         break;
     case '2':
         cout << "Amoxillin 10 strip" << endl;
-        cout << "" << endl;
+        cout << "Amoxillin adalah salah satu merek obat yang digunakan untuk mengatasi infeksi bakteri. Sperti infeksi saluran kemih atau infeksi saluran pernapasan." << endl;
         cout << "Dosis \t= 3x1" << endl;
         cout << "Harga \t= Rp45000" << endl;
         harga2 = 45000;
         break;
     case '3':
         cout << "Cataflam 2 Blister" << endl;
-        cout << "" << endl;
+        cout << "Mengatasi nyeri secara umum atau yang disebabkan oleh radang sendi, missal sakit gigi atau nyeri otot." << endl;
         cout << "Dosis \t= 3x1" << endl;
         cout << "Harga \t= Rp10000" << endl;
         harga3 = 10000;
         break;
     case '4':
         cout << "Aspirin 3 Blister" << endl;
-        cout << "" << endl;
+        cout << "Aspirin adalah obat untuk meredakan nyeri, demam, dan peradangan." << endl;
         cout << "Dosis \t= 3x1" << endl;
         cout << "Harga \t= Rp25000" << endl;
         harga4 = 25000;
         break;
     case '5':
         cout << "Antasida 10 Strip" << endl;
-        cout << "" << endl;
+        cout << "Antasida adalah obat untuk meredakan gejala akibat asam lambung berlebih, seperti nyeri ulu hati, kembung, mual, atau rasa panas di dada." << endl;
         cout << "Dosis \t= 3x1" << endl;
         cout << "Harga \t= Rp14000" << endl;
         harga5 = 14000;
         break;
     case '6':
         utama();
-        break;
+        return 0;
     default:
         cout << "Maaf, Input tidak Valid!" << endl;
+        garis();
+        obat();
+    }
+
+    char beli;
+    cout << "Apakah Anda ingin membeli obat ini? (y/n): ";
+    cin >> beli;
+    if ((beli == 'Y') || (beli == 'y'))
+    {
+        int jumlah;
+        cout << "Jumlah obat yang ingin dibeli:";
+        cin >> jumlah;
+        int totalHarga = 0;
+
+        switch (pilihan)
+        {
+        case '1':
+            totalHarga = harga1 * jumlah;
+            break;
+        case '2':
+            totalHarga = harga2 * jumlah;
+            break;
+        case '3':
+            totalHarga = harga3 * jumlah;
+            break;
+        case '4':
+            totalHarga = harga4 * jumlah;
+            break;
+        case '5':
+            totalHarga = harga5 * jumlah;
+            break;
+        default:
+            break;
+        }
+        cout << "Total harga: Rp" << totalHarga << endl;
+
+        char konfirmasi;
+        cout << "Apakah Anda ingin konfirmasi pembelian? (y/n): ";
+        cin >> konfirmasi;
+
+        if (konfirmasi == 'y' || konfirmasi == 'Y')
+        {
+            cout << "Terima kasih telah melakukan pembelian. Pesanan Anda akan segera diproses." << endl;
+        }
+        else
+        {
+            cout << "Pesanan dibatalkan." << endl;
+        }
+        garis();
+        obat();
+    }
+    else
+    {
         garis();
         obat();
     }
     return 0;
 }
 
-int konsultasi() // Menu konsultasi dokter, belum sama sekali
+int konsultasi() // Menu konsultasi dokter, DONE
 {
     char pilihan;
-    cout << "Konsultasi Dokter" << endl;
-    garis();
-    cout << "Daftar Menu :" << endl;
-    cout << "[1] Kembali" << endl;
-    cout << "Pilih Menu yang anda ingin tuju : ";
-    cin >> pilihan;
-    switch (pilihan)
+    Reservasi reservasi[MAX_RESERVATIONS];
+    int banyakreservasi = 0;
+    while (true)
     {
-    case '1':
-        utama();
-        break;
-    default:
-        cout << "Maaf, Input tidak Valid!" << endl;
+        cout << "Konsultasi Dokter" << endl;
         garis();
-        konsultasi();
+        cout << "Daftar Menu :" << endl;
+        cout << "[1] Reservasi Konsultasi Dokter\n";
+        cout << "[2] Riwayat Reservasi\n";
+        cout << "[3] Kembali" << endl;
+        cout << "Pilih Menu yang anda ingin tuju : ";
+        cin >> pilihan;
+        switch (pilihan)
+        {
+        case '1':
+        {
+            garis();
+            cout << "Daftar Dokter" << endl;
+            for (int i = 0; i < sizeof(daftardokter) / sizeof(daftardokter[0]); ++i)
+            {
+                cout << "[" << i + 1 << "] " << daftardokter[i] << "\n";
+            }
+
+            int pilihdokter;
+            cout << "Pilih Dokter : ";
+            cin >> pilihdokter;
+            if (pilihdokter < 1 || pilihdokter > sizeof(daftardokter) / sizeof(daftardokter[0]))
+            {
+                cout << "Pilihan dokter tidak valid." << endl;
+                continue;
+            }
+
+            garis();
+            cout << "Waktu yang tersedia:" << endl;
+            for (int i = 0; i < sizeof(daftarwaktukonsul) / sizeof(daftarwaktukonsul[0]); i++)
+            {
+                cout << "[" << i + 1 << "] " << daftarwaktukonsul[i] << "\n";
+            }
+
+            int pilihwaktu;
+            cout << "Pilih Waktu Konsultasi: ";
+            cin >> pilihwaktu;
+            if (pilihwaktu < 1 || pilihwaktu > sizeof(daftarwaktukonsul) / sizeof(daftarwaktukonsul[0]))
+            {
+                cout << "Pilihan waktu konsultasi tidak valid." << endl;
+                continue;
+            }
+
+            int pilihbayar;
+            garis();
+            cout << "Pilih jenis pembayaran:\n";
+            cout << "1. QRIS\n";
+            cout << "2. Transfer Bank\n";
+            cout << "3. Tunai\n";
+            cout << "Pilihan (1-3): ";
+            cin >> pilihbayar;
+
+            string carabayar;
+            switch (pilihbayar)
+            {
+            case 1:
+                carabayar = "QRIS";
+                break;
+            case 2:
+                carabayar = "Transfer Bank";
+                break;
+            case 3:
+                carabayar = "Tunai";
+                break;
+            default:
+                cout << "Pilihan cara pembayaran tidak valid" << endl;
+                continue;
+            }
+
+            string hargakonsul;
+            if (pilihdokter == 1)
+            {
+                hargakonsul = "Rp. 50.000";
+            }
+            else if (pilihdokter == 2)
+            {
+                hargakonsul = "Rp. 50.000";
+            }
+            else if (pilihdokter == 3)
+            {
+                hargakonsul = "Rp. 50.000";
+            }
+            else if (pilihdokter == 4)
+            {
+                hargakonsul = "Rp. 200.000";
+            }
+            else if (pilihdokter == 5)
+            {
+                hargakonsul = "Rp. 200.000";
+            }
+            else if (pilihdokter == 6)
+            {
+                hargakonsul = "Rp. 200.000";
+            }
+
+            garis();
+            cout << "Ringkasan Reservasi" << endl;
+            cout << "Dokter \t\t\t: " << daftardokter[pilihdokter - 1] << endl;
+            cout << "Waktu Konsultasi \t: " << daftarwaktukonsul[pilihwaktu - 1] << endl;
+            cout << "Cara Pembayaran \t: " << carabayar << endl;
+            cout << "Harga Konsultasi \t: " << hargakonsul << endl;
+
+            reservasi[banyakreservasi].dokter = daftardokter[pilihdokter - 1];
+            reservasi[banyakreservasi].waktukonsul = daftarwaktukonsul[pilihwaktu - 1];
+            reservasi[banyakreservasi].metodebayar = carabayar;
+            reservasi[banyakreservasi].hargakonsul = hargakonsul;
+            banyakreservasi++;
+            garis();
+        }
+        break;
+        case '2':
+            garis();
+            cout << "Riwayat Reservasi" << endl;
+            garis();
+            if (banyakreservasi == 0)
+            {
+                cout << "Anda belum pernah membuat reservasi." << endl;
+                garis();
+            }
+            else
+            {
+                for (int i = 0; i < banyakreservasi; ++i)
+                {
+                    cout << "Dokter \t\t\t: " << reservasi[i].dokter << endl;
+                    cout << "Waktu Konsultasi \t: " << reservasi[i].waktukonsul << endl;
+                    cout << "Metode Bayar \t\t: " << reservasi[i].metodebayar << endl;
+                    cout << "Harga Konsultasi \t: " << reservasi[i].hargakonsul << endl;
+                    garis();
+                }
+            }
+            break;
+        case '3':
+            utama();
+            return 0;
+        default:
+            cout << "Maaf, Input tidak Valid!" << endl;
+            garis();
+            continue;
+        }
     }
     return 0;
 }
 
-int riwayat_periksa() // Menu riwayat pemeriksaan, fix error do..while
+int riwayat_periksa() // Menu riwayat pemeriksaan, FIXED DONE
 {
     char pilihanPeriksa;
     RekamMedis rekamMedis;
@@ -297,10 +490,12 @@ void menuCariRiwayat(RekamMedis rekamMedis)
         cout << "Nama Pasien: " << rekamMedis.nama << endl;
         cout << "Penyakit: " << rekamMedis.penyakit << endl;
         cout << "Tanggal Pemeriksaan: " << rekamMedis.tanggal << endl;
+        garis();
     }
     else
     {
         cout << "Pasien dengan nama '" << namaCari << "' tidak ditemukan.\n";
+        garis();
     }
 }
 
@@ -314,9 +509,9 @@ void menuUbahRiwayat(RekamMedis &rekamMedis)
 
     if (rekamMedis.nama == namaCari)
     {
-        cout << "1. Ubah Nama Pasien\n";
-        cout << "2. Ubah Penyakit\n";
-        cout << "3. Ubah Tanggal Pemeriksaan\n";
+        cout << "[1] Ubah Nama Pasien\n";
+        cout << "[2] Ubah Penyakit\n";
+        cout << "[3] Ubah Tanggal Pemeriksaan\n";
         cout << "Pilih Opsi: ";
         int pilihan;
         cin >> pilihan;
@@ -327,24 +522,29 @@ void menuUbahRiwayat(RekamMedis &rekamMedis)
             cout << "Nama Baru: ";
             cin.ignore();
             getline(cin, rekamMedis.nama);
+            garis();
             break;
         case 2:
             cout << "Penyakit Baru: ";
             cin.ignore();
             getline(cin, rekamMedis.penyakit);
+            garis();
             break;
         case 3:
             cout << "Tanggal Pemeriksaan Baru: ";
             cin.ignore();
             getline(cin, rekamMedis.tanggal);
+            garis();
             break;
         default:
             cout << "Pilihan tidak valid. Riwayat tetap tidak diubah.\n";
+            garis();
         }
     }
     else
     {
         cout << "Pasien dengan nama '" << namaCari << "' tidak ditemukan.\n";
+        garis();
     }
 }
 
@@ -362,10 +562,12 @@ void menuHapusRiwayat(RekamMedis &rekamMedis)
         rekamMedis.penyakit = "";
         rekamMedis.tanggal = "";
         cout << "Riwayat berhasil dihapus.\n";
+        garis();
     }
     else
     {
         cout << "Pasien dengan nama '" << namaCari << "' tidak ditemukan.\n";
+        garis();
     }
 }
 
@@ -375,12 +577,14 @@ void menuTampilkanRiwayat(RekamMedis rekamMedis)
     if (rekamMedis.nama == "")
     {
         cout << "Riwayat kosong.\n";
+        garis();
     }
     else
     {
-        cout << "Nama Pasien: " << rekamMedis.nama << endl;
-        cout << "Penyakit: " << rekamMedis.penyakit << endl;
-        cout << "Tanggal Pemeriksaan: " << rekamMedis.tanggal << endl;
+        cout << "Nama Pasien \t\t: " << rekamMedis.nama << endl;
+        cout << "Penyakit \t\t: " << rekamMedis.penyakit << endl;
+        cout << "Tanggal Pemeriksaan \t: " << rekamMedis.tanggal << endl;
+        garis();
     }
 }
 
